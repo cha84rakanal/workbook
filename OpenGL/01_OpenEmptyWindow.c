32eb547ed70cf826a613a6a3b9f12718cc8b4d54
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-01-14 16:50:07 +0900

    空のWindowの生成

---
+++ OpenGL/01_OpenEmptyWindow.c
@@ -0,0 +1,18 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+void display(void){
+}
+
+int main(int argc, char *argv[]){
+    glutInit(&argc, argv);
+    glutCreateWindow(argv[0]);
+    glutDisplayFunc(display); //ウィンドウが開かれたり、他のウィンドウによって隠されたウィンドウが再び現れたりして、ウィンドウを再描画する必要があるときに実行する
+    glutMainLoop();
+    return 0;
+}
+
+//cc 01_OpenEmptyWindow.c -framework GLUT -framework GLUT -mmacosx-version-min=10.8
