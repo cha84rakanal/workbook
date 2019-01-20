c7801aab31a9eb7b6e5779c61d354faf754b5088
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-01-20 16:49:43 +0900

    色が補間される

---
+++ OpenGL/04_ColorComplement.c
@@ -0,0 +1,34 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+void display(void){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glColor3d(1.0, 0.0, 0.0); /* 赤 */
+    glVertex2d(-0.9, -0.9);
+    glColor3d(0.0, 1.0, 0.0); /* 緑 */
+    glVertex2d(0.9, -0.9);
+    glColor3d(0.0, 0.0, 1.0); /* 青 */
+    glVertex2d(0.9, 0.9);
+    glColor3d(1.0, 1.0, 0.0); /* 黄 */
+    glVertex2d(-0.9, 0.9);
+    glEnd();
+    glFlush();
+}
+
+void init(void){
+    glClearColor(0.0, 0.0, 1.0, 1.0);
+}
+
+int main(int argc, char *argv[]){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA);
+    glutCreateWindow(argv[0]);
+    glutDisplayFunc(display); //ウィンドウが開かれたり、他のウィンドウによって隠されたウィンドウが再び現れたりして、ウィンドウを再描画する必要があるときに実行する
+    init();
+    glutMainLoop();
+    return 0;
+}
