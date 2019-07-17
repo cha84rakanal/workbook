9095fc29d663949bccb983115055d0e1618497e1
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-07-17 18:15:59 +0900

    Drawできるようにした

--- NDS/mnist/source/main.c
+++ NDS/mnist/source/main.c
@@ -7,10 +7,14 @@
 #include <stdio.h>
 #include <math.h>
 
+#define index(x,y) y*255+x
+
 #include "Validation_inference.h"
 #include "Validation_parameters.h"
 
 int DrawPoint(float x,float y,float r,float reso);
+int DrawPoint2(float x,float y,float r,float reso);
+int DrawCanvas(int *pixel);
 
 //---------------------------------------------------------------------------------
 int main(void) {
@@ -20,26 +24,25 @@ int main(void) {
         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,5,4,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,7,3,0,0,0,0,1,9,6,7,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,4,3,3,5,8,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,1,0,0,2,4,4,3,10,15,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,3,4,1,0,0,0,0,4,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,9,15,0,0,11,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,1,0,0,12,54,110,151,109,20,0,5,0,0,0,0,0,0,0,0,4,0,6,1,0,4,3,0,0,17,0,55,218,250,255,255,205,12,0,8,0,0,0,0,0,0,0,0,0,3,0,0,9,0,0,15,5,8,127,255,240,255,242,191,52,0,2,4,0,0,0,0,0,0,0,0,2,5,0,15,1,0,13,0,24,189,243,255,248,241,228,43,0,0,9,0,0,0,0,0,0,0,0,0,4,0,4,0,3,10,1,133,250,255,255,252,223,38,0,13,11,18,1,0,0,0,0,0,0,0,0,0,0,8,0,10,9,0,101,255,238,255,238,177,27,2,14,0,2,2,0,4,0,0,0,0,0,0,0,0,1,0,0,16,0,78,247,244,255,242,60,0,3,0,3,0,0,0,1,6,0,0,0,0,0,0,0,0,10,0,20,0,76,250,252,255,154,9,20,0,8,0,0,21,1,6,9,0,0,0,0,0,0,0,0,0,0,0,17,152,245,247,253,214,11,7,3,1,0,15,0,0,2,0,0,0,0,0,4,12,0,0,5,7,0,6,79,239,247,247,188,19,0,0,0,0,0,0,0,0,0,0,0,0,2,15,0,0,13,71,0,5,0,87,231,255,255,214,16,12,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,10,0,253,71,7,165,229,255,255,213,23,3,6,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,19,105,198,229,250,255,254,226,111,13,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,14,0,4,74,255,242,255,239,226,22,2,0,4,5,0,0,0,0,0,0,0,0,0,0,0,0,5,0,0,6,0,20,85,255,255,227,85,7,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,1,0,11,0,10,0,81,9,0,0,18,8,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,1,0,1,0,6,0,16,0,8,0,4,0,12,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
     };
 
+    int pixel[48705];
+    for(int i = 0;i < 48705; i++){
+        pixel[i] = 0;
+    }
+    // index(y*255+x)
+
     touchPosition touchXY;
 
-    int touch_x = 0;
-    int touch_y = 0;
+    //set the mode for 2 text layers and two extended background layers
+	videoSetMode(MODE_5_2D); 
 
-	lcdMainOnTop(); //put 3D on top
+	//set the first two banks as background memory and the third as sub background memory
+	//D is not used..if you need a bigger background then you will need to map
+	//more vram banks consecutivly (VRAM A-D are all 0x20000 bytes in size)
+	vramSetPrimaryBanks(	VRAM_A_MAIN_BG_0x06000000, VRAM_B_MAIN_BG_0x06020000, 
+		VRAM_C_SUB_BG , VRAM_D_LCD); 
+
+	//lcdMainOnTop(); //put 3D on top
 	consoleDemoInit(); //setup the sub screen for basic printing
-
-	// Setup the Main screen for 3D 
-	videoSetMode(MODE_0_3D);
-	glInit(); // initialize gl
-	glEnable(GL_ANTIALIAS); // enable antialiasing
-
-    // setup the rear plane
-	glClearColor(0,0,0,31); // BG must be opaque for AA to work
-	glClearPolyID(63); // BG must have a unique polygon ID for AA to work
-	glClearDepth(0x7FFF);
-	
-	// Set our view port to be the same size as the screen
-	glViewport(0,0,255,191);
 	
 	iprintf("Hello World!\n");
 
@@ -68,62 +71,41 @@ int main(void) {
 
     nnablart_validation_free_context(_context);
 
+    int bg = bgInit(3, BgType_Bmp16, BgSize_B16_256x256, 0,0);
+	u16* backBuffer = (u16*)bgGetGfxPtr(bg) + 256*256;
+
+    for(int iy = 0; iy < 190; iy++)
+        for(int ix = 0; ix < 256; ix++) 
+            backBuffer[iy * 256 + ix] = (0x00) | BIT(15);
+
+    backBuffer = (u16*)bgGetGfxPtr(bg);
+    bgSetMapBase(bg, 8);
+
 	while(1) {
 
+        swiWaitForVBlank();
 		scanKeys();
         touchRead(&touchXY);
-
-        glOrtho(-4,4,-3,3,0.1,10);
-        glMatrixMode(GL_MODELVIEW);
-        glPolyFmt(POLY_ALPHA(31) | POLY_CULL_NONE );
 
 		int pressed = keysDown();
 		if(pressed & KEY_START) break;
 
-		//if(pressed & KEY_TOUCH)  {
-		//	touch_x = touchXY.px;
-		//	touch_y = touchXY.py;
-		//}
-
-        glColor3f(1, 1, 1);
-
         iprintf("\x1b[12;12H(%d,%d)      ",touchXY.px,touchXY.py);
-        //iprintf("\x1b[13;12H(%d,%d)      ",(int)(((touchXY.px/255.0f)*4.f)-2.f)*1000,(int)(((touchXY.py/191.0f)*3.f)-1.5f))*1000;
+        iprintf("\x1b[13;12H%d      ",((keysCurrent() & KEY_TOUCH) == 4096));
 
-        glPushMatrix();
-        DrawPoint(
-            ((touchXY.px/255.0f)*2.f)-1.f,
-            -(((touchXY.py/191.0f)*2.f)-1.f),
-            0.04f,
-            10
-        );
-        glPopMatrix(1);
+        if(((keysCurrent() & KEY_TOUCH) == 4096)){
 
-        swiWaitForVBlank();
-        glFlush(0);
+            for(int y = 0;y < 192;y++){
+                for(int x = 0;x < 256;x++){
+                    if((y - touchXY.py)*(y - touchXY.py) + (x - touchXY.px)*(x - touchXY.px) <= 35)
+                        backBuffer[(y) * 256 + (x)] = 0xFFFF;
+                }
+            }
+
+            backBuffer = (u16*)bgGetGfxPtr(bg);
+            bgSetMapBase(bg, 8);
+        }
 
 	}
-
-}
-
-int DrawPoint(float x_pos,float y_pos,float r,float reso) {
-	
-    glLoadIdentity();
-	glTranslatef(x_pos,y_pos,0.0f);
-    float x;
-    float y;
-    glBegin(GL_TRIANGLES);	
-    for (int i = 0; i < reso; i++) {
-        glVertex3f( 0.0f, 0.0f, 0.0f);
-        x = r * cos(2.0 * 3.14 * ((float)i/reso));
-        y = r * sin(2.0 * 3.14 * ((float)i/reso)) *255.0f/191.0f;
-        glVertex3f(x, y, 0.0);
-        x = r * cos(2.0 * 3.14 * ((float)(i+1)/reso));
-        y = r * sin(2.0 * 3.14 * ((float)(i+1)/reso)) *255.0f/191.0f;
-        glVertex3f(x, y, 0.0);
-    }
-    glEnd();
-
-    return TRUE;
 
 }
