cd01ee3b99afb4e170a4b0687d703d8dca78d3f0
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-08-04 00:33:14 +0900

    コメント記述

--- Assembly/ASM/prime_checker_intel.asm
+++ Assembly/ASM/prime_checker_intel.asm
@@ -6,6 +6,6 @@ _main:
     mov eax, 0x1 ;システムコール番号 (sys_exit)
     sub esp, 4 ; esp レジスタから 4 を引く（Mac のシステムコール時のおまじない） 
     int 0x80
-; nasm -f macho32 exit_intel.asm
-; ld -e _main -macosx_version_min 10.8 -o exit_intel exit_intel.o -lSystem
-; ./exit_intel
+; nasm -f macho32 prime_checker_intel.asm
+; ld -e _main -macosx_version_min 10.8 -o prime_checker_intel prime_checker_intel.o -lSystem
+; ./prime_checker_intel
