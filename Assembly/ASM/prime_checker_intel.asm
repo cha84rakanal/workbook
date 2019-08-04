5e528c5899b9c8ae23350dd38053eb30771bb547
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-08-04 22:37:04 +0900

    64bit exitに書き換え

--- Assembly/ASM/prime_checker_intel.asm
+++ Assembly/ASM/prime_checker_intel.asm
@@ -2,10 +2,9 @@ global _main
 
 section .text
 _main:
-    mov ebx, 0x0 ;exit番号
-    mov eax, 0x1 ;システムコール番号 (sys_exit)
-    sub esp, 4 ; esp レジスタから 4 を引く（Mac のシステムコール時のおまじない） 
-    int 0x80
-; nasm -f macho32 prime_checker_intel.asm
+    mov rdi, 0 ; exit(0)
+    mov rax, 0x2000001 ;sys_exit
+    syscall;
+; nasm -f macho64 prime_checker_intel.asm
 ; ld -e _main -macosx_version_min 10.8 -o prime_checker_intel prime_checker_intel.o -lSystem
 ; ./prime_checker_intel
