ac19977f8cf1e923daf84f5770da088ee4f6cbc9
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-08-04 22:37:38 +0900

    64bit 版のリファレンスを追加

--- Assembly/ASM/prime_checker_intel.asm
+++ Assembly/ASM/prime_checker_intel.asm
@@ -8,3 +8,5 @@ _main:
 ; nasm -f macho64 prime_checker_intel.asm
 ; ld -e _main -macosx_version_min 10.8 -o prime_checker_intel prime_checker_intel.o -lSystem
 ; ./prime_checker_intel
+
+; http://thexploit.com/secdev/mac-os-x-64-bit-assembly-system-calls/
