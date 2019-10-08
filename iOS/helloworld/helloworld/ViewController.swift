ce4b67e0df6965d261fb42e7a509e42dac93e87a
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-10-08 12:00:00 +0900

    Hello World iOSプロジェクトのinit

---
+++ iOS/helloworld/helloworld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Tatsuya Minagawa on 2019/11/11.
+//  Copyright © 2019年 Tatsuya Minagawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
